4709427cf5b4f2c3df9706612a426b00f5c3c417
ConnorMeyers <connor.meyers@gmail.com>
2019-05-01 14:09:44 -0700

    GameController
    GameController

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    [Header ("References")]
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private GameObject player;
+
+    [Header ("Gameplay")]
+    [Tooltip("Maximum player size before changing levels")]
+    [SerializeField] private float playerSizeThresh;
+    [Tooltip ("Maximum size of the camera before changing levels")]
+    [SerializeField] private float maxCameraSize;
+
+    // Start is called before the first frame update
+    void Start () {
+        //Subscribe to player scale event for checking when to change levels
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+    
+    // When the player changes in size.
+    private void OnPlayerScale(float size) {
+        // Check if size exceeds maximum player size
+
+        // zoom the camera out
+    }
+}
