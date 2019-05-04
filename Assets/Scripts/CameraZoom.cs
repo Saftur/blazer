faa97dc930d4579427783033c20959fbf2dd36d4
ConnorMeyers <connor.meyers@gmail.com>
2019-05-04 13:16:40 -0700

    LevelTransition Update
    + ObjGrowShrink Transition

---
+++ Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CameraZoom : MonoBehaviour {
+
+    [Header ("References")]
+    [SerializeField] private Camera mainCamera = default;
+    [SerializeField] private Scalable player = default;
+
+    [Header ("Gameplay")]
+    [Tooltip("When player reaches this scale camera will be fully zoomed out")]
+    [SerializeField] private float playerScaleThresh = default;
+    [Tooltip ("Size of camera when fully zoomed out")]
+    [SerializeField] private float maxCameraSize = default;
+
+    // Speed at which camera changes size
+    [Header ("Visuals")]
+    [Tooltip ("Speed at which camera will zoom in/out")]
+    [SerializeField] private float cameraLerpSpeed = 2.0f;
+
+    // Initial size of the camera
+    private float cameraInitSize;
+    // Size camera should be at
+    private float cameraTargetSize;
+
+    // Start is called before the first frame update
+    void Start () {
+        // Subscribe to player scale event for checking when to change levels
+        player.OnScale += OnPlayerScale;
+
+        // Initialize intial camera Size
+        cameraInitSize = mainCamera.orthographicSize;
+        // Initialize camera target size
+        cameraTargetSize = cameraInitSize;
+    }
+    
+    // When the player changes in size.
+    private void OnPlayerScale(float scalar) {
+        // Check if the camera is at max size
+        if(cameraTargetSize >= maxCameraSize) {
+            return;
+        }
+
+        // Set the camera's target zoom
+        float zoom = (scalar - 1 / playerScaleThresh) * (maxCameraSize - cameraInitSize);
+        cameraTargetSize = cameraInitSize + zoom;
+    }
+
+    void Update () {
+        // Zoom the camera towards it's target zoom
+        mainCamera.orthographicSize = Mathf.Lerp (mainCamera.orthographicSize, cameraTargetSize, cameraLerpSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/SceneTransitions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitions : MonoBehaviour {
+
+    [Header ("References")]
+    [SerializeField] private Scalable player = default;
+    [Tooltip ("Name of next level in the game progression (or end screen)")]
+    [SerializeField] private string nextScene = default;
+    [SerializeField] private Animator animator;
+
+    [Header ("Gameplay")]
+    [Tooltip ("When player reaches this scale the scene transition will begin")]
+    [SerializeField] private float playerScaleThresh = default;
+
+    //[Header ("Visuals")]
+    //[Tooltip ("Rather the transition should be played on scene start")]
+    //[SerializeField] private bool startOnLoad = true;
+
+    void Start () {
+        player.OnScale += OnPlayerScaleEvent;
+    }
+
+    private void OnPlayerScaleEvent (float scalar) {
+        if (scalar >= playerScaleThresh) {
+            animator.SetTrigger ("Grow");
+        }
+    }
+
+    // To be called by animator when fade out animation is finisned 
+    public void OnFadeCompleted () {
+        SceneManager.LoadScene (nextScene);
+    }
+}
